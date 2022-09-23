89e9220c47d67a83486b450beb14d0c755187510
Willians Alves <112211987+W1llalves@users.noreply.github.com>
2022-09-23 16:29:44 -0400

    Add files via upload

---
+++ Gerador de Wordlist.py
@@ -0,0 +1,27 @@
+import itertools    #biblioteca quefornece condições para iteração com permutação e combinação.
+
+string = input("String a ser permutado: ")
+
+resultado = itertools.permutations(string, len(string))
+
+for i in resultado:
+    print(''.join(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
