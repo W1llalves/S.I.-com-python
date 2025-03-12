502c49d6549fcef00330427a54a5488bfa442c52
Willians Alves <112211987+W1llalves@users.noreply.github.com>
2025-03-12 16:00:46 -0300

    Update bruteforce_DNS.py

--- bruteforce_DNS.py
+++ bruteforce_DNS.py
@@ -3,12 +3,16 @@
 import dns.resolver
 
 res = dns.resolver.Resolver()
-alvo = "127.0.0.1"
+alvo = "google.com"
 
 try:     #o codigo não vai parar em caso de erro
-    resultado = res.resolve(alvo, "A")
+    resultado = res.resolve(alvo,"A")
     for ip in resultado:
         print(alvo, "->", ip)
 
-except:
-        pass  #ignorar
+except dns.resolver.NoAnswer:
+    print(f"Sem resposta para {alvo}")
+except dns.resolver.NXDOMAIN:
+    print(f"Domínio {alvo} não encontrado")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
